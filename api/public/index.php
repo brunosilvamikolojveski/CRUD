955530dc03243033e7ce84d15b2075751bb5b097
Bruno <bruno.mikolojveski@escola.pr.gov.br>
2026-09-22 08:01:59 -0300

    Atualizações e migração

--- api/public/index.php
+++ api/public/index.php
@@ -5,9 +5,10 @@ require_once __DIR__ . '/../config/config.php';
 $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
 
 in_array($origin, $allowedOrigins) ?
-    header("Access-Control-Allow-Origin: $origin") : null;
+header("Access-Control-Allow-Origin: $origin") : null;
 header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
+header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(204);
